2fca1605aee470cf0db18a645465c463a3bca436
Tervi test <Tervi@list.ru>
2023-07-31 12:38:44 +0300

    one version

--- src/test/java/ru/netology/service/AppCardDelivery.java
+++ src/test/java/ru/netology/service/AppCardDelivery.java
@@ -20,15 +20,15 @@ public class AppCardDelivery {
     public void shouldBeSuccess() {
         open("http://localhost:9999/");
         $("[data-test-id='city'] input").setValue("Санкт-Петербург");
-        String date =generate(4, "dd.MM.yyyy");
+        String date =generate(7, "dd.MM.yyyy");
         $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
         $("[data-test-id='date'] input").sendKeys(date);
         $("[data-test-id='name'] input").setValue("Иванов Иван");
         $("[data-test-id='phone'] input").setValue("+79110000000");
         $("data-test-id='agreement'").click();
         $("button").click();
-        $(".notification__content")
-                .shouldBe(Condition.visible, Duration.ofSeconds(15))
+        $("notification_content")
+                .shouldBe(Condition.visible, Duration.ofSeconds(30))
                 .shouldHave(Condition.exactText("Встреча успешно забронирована на " + date));
 
     }
