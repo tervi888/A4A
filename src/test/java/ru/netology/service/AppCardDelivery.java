b3a22835b3308ff124a71002c9325404103f7f0e
Tervi test <Tervi@list.ru>
2023-07-31 11:07:04 +0300

    one version

---
+++ src/test/java/ru/netology/service/AppCardDelivery.java
@@ -0,0 +1,36 @@
+package ru.netology.service;
+
+import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class AppCardDelivery {
+
+    private String generate(int addDays, String pattern) {
+        return LocalDate.now().plusDays(addDays).format(DateTimeFormatter.ofPattern(pattern));
+    }
+    @Test
+    public void shouldBeSuccess() {
+        open("http://localhost:9999/");
+        $("[data-test-id='city'] input").setValue("Санкт-Петербург");
+        String date =generate(4, "dd.MM.yyyy");
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
+        $("[data-test-id='date'] input").sendKeys(date);
+        $("[data-test-id='name'] input").setValue("Иванов Иван");
+        $("[data-test-id='phone'] input").setValue("+79110000000");
+        $("data-test-id='agreement'").click();
+        $("button").click();
+        $(".notification__content")
+                .shouldBe(Condition.visible, Duration.ofSeconds(15))
+                .shouldHave(Condition.exactText("Встреча успешно забронирована на " + date));
+
+    }
+
+}
